5ecbc07f6fcc8cbf3289e1f7e87b299c23c52081
Starta Architect <81386301+starta-architect@users.noreply.github.com>
2019-10-10 20:14:24 +0000

    feat: core js sources;

---
+++ js/config.js
@@ -0,0 +1,25 @@
+/*
+*       ███████╗████████╗ █████╗ ██████╗ ████████╗ █████╗
+*       ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗
+*       ███████╗   ██║   ███████║██████╔╝   ██║   ███████║
+*       ╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ██╔══██║
+*       ███████║   ██║   ██║  ██║██║  ██║   ██║   ██║  ██║
+*       ╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝
+*  ███████╗██╗   ██╗███████╗████████╗███████╗███╗   ███╗███████╗
+*  ██╔════╝╚██╗ ██╔╝██╔════╝╚══██╔══╝██╔════╝████╗ ████║██╔════╝
+*  ███████╗ ╚████╔╝ ███████╗   ██║   █████╗  ██╔████╔██║███████╗
+*  ╚════██║  ╚██╔╝  ╚════██║   ██║   ██╔══╝  ██║╚██╔╝██║╚════██║
+*  ███████║   ██║   ███████║   ██║   ███████╗██║ ╚═╝ ██║███████║
+*  ╚══════╝   ╚═╝   ╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚══════╝
+===================================================================
+* Start[001] Website PRO | v1.0.0 | 2019-08-22
+=================================================================
+* Product Page: https://startasystems.github.io/start-website-pro/
+* Copyright 2019 | Starta Systems (https://startasystems.github.io/)
+* Coded by @starta.architect
+=================================================================
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+**/
+
+window.$config={"subscribe-form":{error1:"Desculpe, ocorreu um erro no seu cadastro. Tente novamente ou envie um e-mail para starta.systems@protonmail.com",error2:"E-mail inválido. Não foi possível verificar a autenticidade desse email. Por favor preencha com outro email válido.",alert1:"Enviando . . .",success:"Inscrição realizada com sucesso 🚀"},"contact-form":{error1:"Desculpe, ocorreu um erro no seu cadastro. Tente novamente ou envie um e-mail para starta.systems@protonmail.com",error2:"E-mail inválido. Não foi possível verificar a autenticidade desse email. Por favor preencha com outro email válido.",alert1:"Por favor preencha todos os campos marcados como obrigatórios.",alert2:"Enviando . . .",success:"Mensagem enviada com sucesso 🚀"}},$("body").on("starWebsite",function(){});
